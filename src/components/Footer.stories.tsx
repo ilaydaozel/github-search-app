0e619b346e283b457e61ae984bf808737804e186
ilaydaozel <ilayda.ozel@im-par.de>
2024-09-15 19:17:30 +0300

    Create Footer story

---
+++ src/components/Footer.stories.tsx
@@ -0,0 +1,12 @@
+import Footer from './Footer';
+
+export default {
+  component: Footer,
+  title: 'Components/Footer',
+  tags: ['autodocs'],
+  args: {},
+};
+
+export const Default = {
+  args: {},
+};
