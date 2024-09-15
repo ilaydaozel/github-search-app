7bcf6f90009f4aee52a10ed1ac9960c741aa6647
ilaydaozel <ilayda.ozel@im-par.de>
2024-09-15 19:03:10 +0300

    Get the current year in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <footer className="text-center text-textColor-500 p-6">
-      <p>Made by <a className="" href="https://ilayda-ozel.vercel.app/" target="_blank" rel="noopener noreferrer"> İlayda Özel </a></p>
-      <p>© 2024 GitHub Repo Search</p>
+      <p>Made by <a className="font-semibold" href="https://ilayda-ozel.vercel.app/" target="_blank" rel="noopener noreferrer"> İlayda Özel </a></p>
+      <p>©{new Date().getFullYear()} GitHub Repo Search</p>
     </footer>
   );
 };
