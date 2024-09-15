72056cf2aca6da61b5ce7e11b888b296dadd9c39
ilaydaozel <ilayda.ozel@im-par.de>
2024-09-15 16:49:20 +0300

    Add global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {  
+      colors: {
+        ...colors,
+        primary: '#dfc5c4',
+        secondary: "#598277",
+        bgColor: "#ffffff",
+        textColor: colors.gray,
+    },
+  },
   },
   plugins: [],
 }
