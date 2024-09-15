55e5ff20bf15790d0ff25ea648625379b44864d1
ilaydaozel <ilayda.ozel@im-par.de>
2024-09-16 00:03:04 +0300

    Add suspense to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import store, { fetchGithubUsers } from '../lib/store';
-import UserCard from '../components/Cards/UserCard/UserCard';
 import SearchBar from '../components/SearchBar/SearchBar';
 import { GithubUser } from '../types';
+
+// Lazy load the UserList component
+const UserList = lazy(() => import('../components/Lists/UserList/UserList'));
 
 const HomePage = () => {
   const dispatch = useDispatch<typeof store.dispatch>();
   const { users, status, error } = useSelector((state: ReturnType<typeof store.getState>) => state.users);
   const [searchTerm, setSearchTerm] = useState('');
 
-  // Fetch users when component mounts
   useEffect(() => {
     dispatch(fetchGithubUsers());
   }, [dispatch]);
@@ -24,23 +25,10 @@ const HomePage = () => {
     setSearchTerm(term);
   };
 
-  // Handling loading state
   if (status === 'loading') {
-    return (
-      <div className="list-items" data-testid="loading" key="loading">
-        {[...Array(6)].map((_, i) => (
-          <div key={i} className="loading-item">
-            <span className="glow-checkbox" />
-            <span className="glow-text">
-              <span>Loading</span> <span>user</span> <span>profile</span>
-            </span>
-          </div>
-        ))}
-      </div>
-    );
+    return <div>Loading users...</div>;
   }
 
-  // Handling error state
   if (status === 'failed') {
     return <div className="error-message">Error: {error}</div>;
   }
@@ -52,17 +40,14 @@ const HomePage = () => {
         <SearchBar placeholder="Search GitHub user..." onSearch={handleSearch} />
       </div>
 
-
-      {/* Filtered Users List */}
-      <div className="w-full grid grid-cols-2 gap-4 place-content-center">
-        {filteredUsers.length > 0 ? (
-          filteredUsers.map((user: GithubUser) => (
-            <UserCard key={user.id} user={user} />
-          ))
-        ) : (
-          <div className="text-lefttext-gray-500">No users found.</div>
-        )}
-      </div>
+      {/* Suspense for UserList */}
+      <Suspense fallback={<div>Loading user list...</div>}>
+          {filteredUsers.length > 0 ? (
+            <UserList users={filteredUsers} />
+          ) : (
+            <div className="text-gray-500">No users found.</div>
+          )}
+      </Suspense>
     </div>
   );
 };
