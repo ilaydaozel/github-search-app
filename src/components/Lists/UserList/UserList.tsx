8ee8ab3f7a44d5927fce2e8ae8e9e785cf43d55d
ilaydaozel <ilayda.ozel@im-par.de>
2024-09-15 22:29:22 +0300

    Update UserList

--- src/components/Lists/UserList/UserList.tsx
+++ src/components/Lists/UserList/UserList.tsx
@@ -1,39 +1,15 @@
-// UsersList.tsx
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import store, { fetchGithubUsers } from '../../../lib/store';
+import UserCard from '../../Cards/UserCard/UserCard';
+import { GithubUser } from '../../../types';
 
+const UsersList = ({ users }: { users: GithubUser[]}) => {
+  
 
-const UsersList: React.FC = () => {
-const dispatch = useDispatch<typeof store.dispatch>();
-  // We're retrieving the error field from our updated store
-  const { users, status, error } = useSelector((state: ReturnType<typeof store.getState>) => state.users);
-  // The useEffect triggers the data fetching when the component is mounted
-  useEffect(() => {
-    dispatch(fetchGithubUsers());
-  }, []);
-  console.log("users", users)
-  if (status === 'loading') {
-    return <div>Loading...</div>;
-  }
-
-  if (status === 'failed') {
-    return <div>Error: {error}</div>;
-  }
-
+  // Rendering the list of users
   return (
-    <div>
-      <h1>GitHub Users</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            <img src={user.avatar_url} alt={user.login} width="50" />
-            <a href={user.html_url} target="_blank" rel="noopener noreferrer">
-              {user.login}
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div className="grid grid-cols-2 gap-4">
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 };
