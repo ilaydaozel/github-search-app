6525d97f38814dba4782678233dd964db6ab5082
ilaydaozel <ilayda.ozel@im-par.de>
2024-09-16 00:00:30 +0300

    Refactor user list layout

--- src/components/Lists/UserList/UserList.tsx
+++ src/components/Lists/UserList/UserList.tsx
@@ -5,7 +5,7 @@ const UserList = ({ users }: { users: GithubUser[]}) => {
   
   // Rendering the list of users
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4">
       {users.map((user) => (
         <UserCard key={user.id} user={user} />
       ))}
