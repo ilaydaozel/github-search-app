47667e8935372e90ce3abe98d100b159296a0268
ilaydaozel <ilayda.ozel@im-par.de>
2024-09-16 03:09:30 +0300

    Change navbar link color when hovered

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
       <nav className="flex justify-between items-center bg-bgColor px-8 py-4 w-full shadow-sm">
-        <a href="/" className="text-textColor-600 font-semibold text-md transition duration-300 hover:translate-y-1">Home</a>
+        <a href="/" className="text-textColor-600 font-semibold text-md transition-transform duration-500 hover:translate-y-1 hover:text-primary">Home</a>
       </nav>
     )
 };
