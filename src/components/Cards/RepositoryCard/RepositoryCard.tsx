471b403b5c4ee73f4127e8be18552afae287db0a
ilaydaozel <ilaydaoozel@gmail.com>
2024-09-16 18:34:44 +0300

    Improve accessibility

--- src/components/Cards/RepositoryCard/RepositoryCard.tsx
+++ src/components/Cards/RepositoryCard/RepositoryCard.tsx
@@ -11,8 +11,8 @@ const RepositoryCard: React.FC<RepositoryCardProps> = ({ repository }) => {
     <Link to={repository.html_url} className="bg-bgColor flex flex-col justify-between gap-2 shadow-md rounded-md p-4 border border-textColor-200 transition-transform duration-500 hover:translate-y-1 break-words">
       <h2 className="text-lg font-semibold text-primary">{repository.name}</h2>
       <div className="text-sm text-textColor-600">{repository.description}</div>
-      <div className='flex gap-2 text-textColor-400 text-xs items-center'>
-        <p className="bg-textColor-300 rounded-md px-2 py-1 text-bgColor">{repository.private ? 'Private' : 'Public'}</p>
+      <div className='flex gap-2 text-textColor-500 text-xs items-center'>
+        <p className="bg-textColor-500 rounded-md px-2 py-1 text-bgColor">{repository.private ? 'Private' : 'Public'}</p>
         <p>{repository.language}</p>
       </div>  
     </Link>
--- src/components/DropdownFilter/DropdownFilter.tsx
+++ src/components/DropdownFilter/DropdownFilter.tsx
@@ -8,19 +8,29 @@ interface DropdownFilterProps {
 
 const DropdownFilter = ({ id, label, options, selectedValue, onChange }: DropdownFilterProps) => {
   return (
+    <div className="relative">
+      <label
+        id={`${id}-label`}  // Ensure the label has an id
+        htmlFor={id}
+        className="text-sm font-medium text-gray-700 hidden"
+      >
+        {label}
+      </label>
       <select
         id={id}
         value={selectedValue}
         onChange={(e) => onChange(e.target.value)}
-        className="bg-bgColor border border-textColor-300 md:text-base text-sm rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-primary focus:borderprimary p-2"
+        className="bg-bgColor border border-textColor-300 md:text-base text-sm rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary p-2"
+        aria-labelledby={`${id}-label`}  // Ensure the select element references the label's id
         data-testid={`${id}-filter`}
-      >        
+      >
         {options.map((option) => (
           <option key={option.value} value={option.value}>
-            {label} {option.label}
+            {option.label}
           </option>
         ))}
       </select>
+    </div>
   );
 };
 
