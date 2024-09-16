b5c3e080bfb511fb1948aa5255257ff1fca7806b
ilaydaozel <ilaydaoozel@gmail.com>
2024-09-16 12:51:49 +0300

    Integrate react routers with storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,16 @@
 import type { Preview } from "@storybook/react";
-import '../src/index.css'
+import '../src/index.css';
+import { withRouter, reactRouterParameters } from 'storybook-addon-remix-react-router';
 
+// Define your decorators and parameters
 const preview: Preview = {
+  decorators: [withRouter],
   parameters: {
+    reactRouter: reactRouterParameters({ 
+      location: {
+      pathParams: { username: 'mojombo' },
+    },
+    routing: { path: '/users/:username' }, }),
     controls: {
       matchers: {
         color: /(background|color)$/i,
