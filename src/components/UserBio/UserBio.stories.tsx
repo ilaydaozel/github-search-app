c4ef0c7f4054d3ea456c761e1079a4d257559a1b
ilaydaozel <ilaydaoozel@gmail.com>
2024-09-16 13:05:42 +0300

    Create user bio story

---
+++ src/components/UserBio/UserBio.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react';
+import UserBio from './UserBio';
+import { GithubUser } from '../../types';
+
+// Mock data for the story
+const mockUser: GithubUser = {
+    login: 'octocat',
+    avatar_url: 'https://avatars.githubusercontent.com/u/583231?v=4',
+    html_url: 'https://github.com/octocat',
+    id: 0,
+    repos_url: ''
+};
+
+export default {
+  title: 'Components/UserBio',
+  component: UserBio,
+  args: {
+    user: mockUser,
+  },
+};
+
+// Default UserBio Story
+export const Default= {
+  args: {
+    user: mockUser,
+  },
+};
+
+// Story for when user data is not available (e.g., loading state)
+export const Loading = {
+  args: {
+    user: undefined,
+  },
+};
