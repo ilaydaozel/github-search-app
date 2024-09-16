9a9251c6c6e6e1bea07a3bd2130f88033921bdb3
ilaydaozel <ilayda.ozel@im-par.de>
2024-09-16 03:10:01 +0300

    Add UserBio to user page

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -4,6 +4,7 @@ import store, { fetchGithubRepositories, setFilterType, setFilterLanguage } from
 import { useParams } from 'react-router-dom';
 import DropdownFilter from '../components/DropdownFilter/DropdownFilter';
 import SearchBar from '../components/SearchBar/SearchBar';
+import UserBio from '../components/UserBio/UserBio';
 
 const RepositoryList = lazy(() => import('../components/Lists/RepositoryList/RepositoryList'));
 
@@ -35,8 +36,7 @@ const UserPage = () => {
 
     // Apply search term filter
     result = result.filter(repo =>
-      repo.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      repo.description?.toLowerCase().includes(searchTerm.toLowerCase())
+      repo.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     setFilteredRepos(result);
@@ -55,54 +55,55 @@ const UserPage = () => {
   }
 
   return (
-    <div className='relative container w-4/5 flex flex-col flex-grow items-center justify-start gap-16 py-8'>
-      <h1>{username}'s Repositories</h1>
-      <div className='w-full flex md:flex-row md:items-center flex-col md:gap-8 gap-2 items-end justify-end'>
-        {/* Search Bar */}
-        <SearchBar placeholder="Search repositories..." onSearch={handleSearch} />
+    <div className='relative container w-5/6 flex-grow py-16'>
+      <div className='w-full grid grid-cols-1 lg:grid-cols-4 gap-4'>
+         {/* Conditionally render UserBio only if the owner exists */}
+         {repos[0]?.owner && <UserBio user={repos[0].owner} />}
+        <div className='flex flex-col items-center justify-start gap-16 lg:col-span-3'>
+            <div className='w-full flex md:flex-row md:items-center flex-col md:gap-8 gap-2 items-end justify-end'>
+                {/* Search Bar */}
+                <SearchBar placeholder="Search repositories..." onSearch={handleSearch} />
 
-        {/* Filters */}
-        <div className="w-fit flex gap-2">
-            <DropdownFilter
-            id="repo-type"
-            label="Type:"
-            options={[
-                { value: 'all', label: 'All' },
-                { value: 'public', label: 'Public' },
-                { value: 'private', label: 'Private' },
-            ]}
-            selectedValue={filterType}
-            onChange={(value) => dispatch(setFilterType(value as 'all' | 'public' | 'private'))}
-            />
-
-            <DropdownFilter
-            id="repo-language"
-            label="Language:"
-            options={[
-                { value: 'all', label: 'All' },
-                { value: 'JavaScript', label: 'JavaScript' },
-                { value: 'TypeScript', label: 'TypeScript' },
-                { value: 'Python', label: 'Python' },
-                { value: 'CSS', label: 'CSS' },
-                { value: 'Erlang', label: 'Erlang' },
-                { value: 'Ruby', label: 'Ruby' },
-                { value: 'Jupyter Notebook', label: 'Jupyter Notebook' },
-            ]}
-            selectedValue={filterLanguage}
-            onChange={(value) => dispatch(setFilterLanguage(value))}
-            />
-        </div>
-        </div>
-      
-      <Suspense fallback={<div>Loading repositories...</div>}>
-        <div>
-          {filteredRepos.length > 0 ? (
-            <RepositoryList repositories={filteredRepos} />
-          ) : (
-            <div className="text-gray-500">No repositories found.</div>
-          )}
-        </div>
-      </Suspense>
+                {/* Filters */}
+                <div className="w-fit flex gap-2 flex-wrap md:flex-nowrap justify-end">
+                    <DropdownFilter
+                        id="repo-type"
+                        label="Type:"
+                        options={[
+                            { value: 'all', label: 'All' },
+                            { value: 'public', label: 'Public' },
+                            { value: 'private', label: 'Private' },
+                        ]}
+                        selectedValue={filterType}
+                        onChange={(value) => dispatch(setFilterType(value as 'all' | 'public' | 'private'))}
+                    />
+                    <DropdownFilter
+                        id="repo-language"
+                        label="Language:"
+                        options={[
+                            { value: 'all', label: 'All' },
+                            { value: 'JavaScript', label: 'JavaScript' },
+                            { value: 'TypeScript', label: 'TypeScript' },
+                            { value: 'Python', label: 'Python' },
+                            { value: 'CSS', label: 'CSS' },
+                            { value: 'Erlang', label: 'Erlang' },
+                            { value: 'Ruby', label: 'Ruby' },
+                        ]}
+                        selectedValue={filterLanguage}
+                        onChange={(value) => dispatch(setFilterLanguage(value))}
+                    />
+                </div>
+            </div>
+            
+            <Suspense fallback={<div>Loading repositories...</div>}>
+                {filteredRepos.length > 0 ? (
+                    <RepositoryList repositories={filteredRepos} />
+                ) : (
+                    <div className="text-gray-500">No repositories found.</div>
+                )}
+            </Suspense>
+            </div>
+      </div>
     </div>
   );
 };
